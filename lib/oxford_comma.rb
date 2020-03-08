8d555abfda21c227276037a8cddd05ac008e698e
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-08 00:08:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
-  if array.size == 1
+  if array.length == 1
     array.join(",")
-  elsif array.size == 2
+  elsif array.length == 2
     array.join(" and ")
-  elsif 
+  elsif array.length >= 3
     array(-1 = " and #{array[-1]}")
     array.join(", ")
+  else
+    nil
   end
 end
